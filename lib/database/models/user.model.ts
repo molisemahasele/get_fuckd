31144957789dca0347b7fd1fa6ce2fb56f9f68bb
Molise <61286622+molisemahasele@users.noreply.github.com>
2024-03-16 04:08:18 -0700

    Update user.model.ts

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -36,6 +36,6 @@ const UserSchema = new Schema({
   },
 });
 
-const User = models?.User || model("User", UserSchema);
+const User = models?.User || model("collection1", UserSchema);
 
 export default User;
